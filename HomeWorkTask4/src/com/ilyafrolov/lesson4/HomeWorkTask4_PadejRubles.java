3f87c5a986a7d135020a34b831d7510dbfa76ab4
ilya-frolov <ilyafrolov0711@gmail.com>
2022-06-28 11:42:01 +0300

    lesson 4: Task for "printing the rubles value with the word "rubley" in correct padej" is completed.

---
+++ HomeWorkTask4/src/com/ilyafrolov/lesson4/HomeWorkTask4_PadejRubles.java
@@ -0,0 +1,38 @@
+package com.ilyafrolov.lesson4;
+
+import java.util.Random;
+
+public class HomeWorkTask4_PadejRubles {
+    public static void main(String[] args) {
+
+        /*
+        Имеется целове число (задать с помощью Random rand = new Random(); int x =
+        rand.nextInt()).
+        Это число – количесво денег в рублях. Вывести это число,
+        добавив к нему слово «рублей» в правильном падеже.
+         */
+
+        //Step 1: obtaining the natural given number
+        Random rand = new Random();
+        int x = rand.nextInt();
+
+        //Step 2: supplying positive numbers
+        if (x <= 0) {
+            x = x * (-1);
+        }
+
+        //Step 3: printing the rubles value with the word "rubley" in correct padej
+        String rubl = "rubl";
+
+        String number = String.valueOf(x);
+        char[] digit = number.toCharArray();
+
+        for (int i = digit.length - 1; i < digit.length; i++) {
+            switch (digit[i]) {
+                case '1' -> System.out.println("" + x + " " + rubl + "'");
+                case '2', '3', '4' -> System.out.println("" + x + " " + rubl + "ya");
+                default -> System.out.println("" + x + " " + rubl + "ej");
+            }
+        }
+    }
+}

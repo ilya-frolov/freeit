650aa50034e5106e9d41de96c5279819c4bdc21c
ilya-frolov <ilyafrolov0711@gmail.com>
2022-06-30 22:51:29 +0300

    lesson 4: Task "printing the rubles value with the word "rubley" in correct padej" numbers 11 and 12 included into consideration.

--- HomeWorkTask4/src/com/ilyafrolov/lesson4/HomeWorkTask4_PadejRubles.java
+++ HomeWorkTask4/src/com/ilyafrolov/lesson4/HomeWorkTask4_PadejRubles.java
@@ -15,10 +15,10 @@ public class HomeWorkTask4_PadejRubles {
 
         //Step 1: obtaining the natural given number
         Random rand = new Random();
-        int x = rand.nextInt();
+        int x = rand.nextInt(2000);
 
         //Step 2: supplying positive numbers
-        if (x <= 0) {
+        if (x < 0) {
             x = x * (-1);
         }
 
@@ -28,11 +28,27 @@ public class HomeWorkTask4_PadejRubles {
         String number = String.valueOf(x);
         char[] digit = number.toCharArray();
 
-        for (int i = digit.length - 1; i < digit.length; i++) {
-            switch (digit[i]) {
-                case '1' -> System.out.println("" + x + " " + rubl + "'");
-                case '2', '3', '4' -> System.out.println("" + x + " " + rubl + "ya");
-                default -> System.out.println("" + x + " " + rubl + "ej");
+        if (x > 10) {
+            for (int i = digit.length - 2; i < digit.length - 1; i++) {
+                if (digit[digit.length - 2] == '1') {
+                    System.out.println("" + x + " " + rubl + "ej");
+                } else {
+                    for (int j = digit.length - 1; j < digit.length; j++) {
+                        switch (digit[j]) {
+                            case '1' -> System.out.println("" + x + " " + rubl + "'");
+                            case '2', '3', '4' -> System.out.println("" + x + " " + rubl + "ya");
+                            default -> System.out.println("" + x + " " + rubl + "ej");
+                        }
+                    }
+                }
+            }
+        } else {
+            for (int i = digit.length - 1; i < digit.length; i++) {
+                switch (digit[i]) {
+                    case '1' -> System.out.println("" + x + " " + rubl + "'");
+                    case '2', '3', '4' -> System.out.println("" + x + " " + rubl + "ya");
+                    default -> System.out.println("" + x + " " + rubl + "ej");
+                }
             }
         }
     }

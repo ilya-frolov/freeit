bf24308af4c586c231668a8608d4288c62b77573
ilya-frolov <ilyafrolov0711@gmail.com>
2022-07-01 18:01:08 +0300

    lesson 3: Task "print the given in seconds time in different values" deleted not needed comments.

--- HomeWorkTask3/src/com/ilyafrolov/lesson3/HomeWorkTask3_TimeTask.java
+++ HomeWorkTask3/src/com/ilyafrolov/lesson3/HomeWorkTask3_TimeTask.java
@@ -21,30 +21,30 @@ public class HomeWorkTask3_TimeTask {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the time period in seconds, please: ");
-        int periodInSec = input.nextInt(); //given time period in seconds
+        int periodInSec = input.nextInt();
 
         while (periodInSec < 0) {
             Scanner input2 = new Scanner(System.in);
             System.out.println("Enter the positive value for time period in seconds, please: ");
-            periodInSec = input2.nextInt(); //given time period in seconds
+            periodInSec = input2.nextInt();
         }
 
-        int periodInMin = periodInSec / 60;                      //given time period in minutes without the rest of time in seconds
-        int periodRestSec = periodInSec - periodInMin * 60;      //the rest of time in seconds to full minutes number included into given time period
+        int periodInMin = periodInSec / 60;
+        int periodRestSec = periodInSec - periodInMin * 60;
 
-        int periodInHours = periodInMin / 60;                    //given time period in hours
-        int periodRestMin = periodInMin - periodInHours * 60;    //the rest of time in minutes to full hours number included into given time period
+        int periodInHours = periodInMin / 60;
+        int periodRestMin = periodInMin - periodInHours * 60;
 
-        int periodInDays = periodInHours / 24;                   //given time period in days
-        int periodRestHours = periodInHours - periodInDays * 24; //the rest of time in hours to full days number included into given time period
+        int periodInDays = periodInHours / 24;
+        int periodRestHours = periodInHours - periodInDays * 24;
 
-        int periodInWeeks = periodInDays / 7;                    //given time period in weeks
-        int periodRestDays = periodInDays - periodInWeeks * 7;   //the rest of time in days to full weeks number included into given time period
+        int periodInWeeks = periodInDays / 7;
+        int periodRestDays = periodInDays - periodInWeeks * 7;
 
         System.out.println("A) " + periodInMin + " min " + periodRestSec + " s"); // gine time period in minutes and seconds
-        System.out.println("B) " + periodInHours + " hours " + periodRestMin + " min " + periodRestSec + " s"); // gine time period in hours, minutes and seconds
-        System.out.println("C) " + periodInDays + " days " + periodRestHours + " hours " + periodRestMin + " min " + periodRestSec + " s"); // gine time period in days, hours, minutes and seconds
-        System.out.println("D) " + periodInWeeks + " weeks " + periodRestDays + " days " + periodRestHours + " hours " + periodRestMin + " min " + periodRestSec + " s"); // gine time period in weeks, days, hours, minutes and seconds
+        System.out.println("B) " + periodInHours + " hours " + periodRestMin + " min " + periodRestSec + " s");
+        System.out.println("C) " + periodInDays + " days " + periodRestHours + " hours " + periodRestMin + " min " + periodRestSec + " s");
+        System.out.println("D) " + periodInWeeks + " weeks " + periodRestDays + " days " + periodRestHours + " hours " + periodRestMin + " min " + periodRestSec + " s");
 
         System.out.println("\nThe program is completed successfully!");
     }

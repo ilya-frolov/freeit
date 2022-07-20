4310a33c4a3809ae5742a80b816e0ab72f1051c9
ilya-frolov <ilyafrolov0711@gmail.com>
2022-07-20 20:28:43 +0300

    lesson7: Task "create class for Notebook which can save tasks and give frequency vocabulary for certain ones" added sorting tasks by date.

--- HomeWorkTask7/src/com/ilyafrolov/lesson7/Notebook.java
+++ HomeWorkTask7/src/com/ilyafrolov/lesson7/Notebook.java
@@ -1,24 +1,31 @@
 package com.ilyafrolov.lesson7;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Notebook {
 
     /*
     Создать класс записной книжки, в котором хранятся записи дел.
     Создать в этом классе метод, который выводит частотный словарь
     для определенной записи (запись можно определить по номеру/индексу).
+    Отсортировать записи, хранящиеся в записной книжке
+    по времени их добавления и вывести в этом порядке в консоль.
      */
 
     public String[] tasks;
     public int counter;
+    public Date[] curDate;
 
     public Notebook(int numberOfTasks) {
         tasks = new String[numberOfTasks];
+        curDate = new Date[tasks.length];
     }
 
     public String toString() {
         System.out.println("Existing tasks: ");
         for (int i = 0; i < tasks.length; i++) {
-            System.out.println("" + i + ". " + tasks[i]);
+            System.out.println("" + i + ". " + tasks[i] + "  " + curDate[i]);
         }
         return "";
     }
@@ -27,6 +34,7 @@ public class Notebook {
         for (int i = 0; i < tasks.length; i++) {
             if (tasks[i] == null) {
                 tasks[i] = newTask;
+                curDate[i] = Calendar.getInstance().getTime();
                 counter++;
                 break;
             } else {
@@ -67,6 +75,28 @@ public class Notebook {
                 System.out.println("" + words2[j] + " - " + numberOfRepeats2[j] + " time(s)");
                 j++;
             }
+        }
+    }
+
+    public void reWriteTask(int numberOfTask, String newTask) {
+        tasks[numberOfTask - 1] = newTask;
+        curDate[numberOfTask - 1] = Calendar.getInstance().getTime();
+    }
+
+    public void sortByDate() {
+        for (int i = 0; i < curDate.length - 1; i++) {
+            int last = i;
+            for (int j = i + 1; j < curDate.length; j++) {
+                if (curDate[last].after(curDate[j])) {
+                    last = j;
+                }
+            }
+            String tempor = tasks[i];
+            tasks[i] = tasks[last];
+            tasks[last] = tempor;
+            Date tempor2 = curDate[i];
+            curDate[i] = curDate[last];
+            curDate[last] = tempor2;
         }
     }
 }

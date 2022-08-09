e731bf960c9e0353835a4ef2aeee45c1da4087f7
ilya-frolov <ilyafrolov0711@gmail.com>
2022-08-09 11:27:22 +0300

    lesson5: Task 17 "create 2d array in the form of butterfly" corrected code for various array sizes.

--- HomeWorkTask5/src/com/ilyafrolov/lesson5/HomeWorkTask5_ArraysButterfly.java
+++ HomeWorkTask5/src/com/ilyafrolov/lesson5/HomeWorkTask5_ArraysButterfly.java
@@ -13,23 +13,25 @@ public class HomeWorkTask5_ArraysButterfly {
         1 1 1 1 1
          */
 
-        //Step 1: Creation of the array upper part
-        int[][] butfly = new int[5][5];
+        int size = 5;
+        int[][] butfly = new int[size][size];
+
+        //Step 1: Creation of the upper array part
         int n = 0;
-        for (int i = 0; i < butfly.length - 2; i++) {
+        for (int i = 0; i < butfly.length - butfly.length / 2; i++) {
             for (int i1 = n; i1 < butfly[i].length - n; i1++) {
                 butfly[i][i1] = 1;
             }
             n += 1;
         }
 
-        //Step 2: Creation of the array bottom part
-        n = 1;
-        for (int i = butfly.length - 2; i < butfly.length; i++) {
+        //Step 2: Creation of the bottom array part
+        n = 0;
+        for (int i = butfly.length - 1; i >= butfly.length - butfly.length / 2; i--) {
             for (int i1 = n; i1 < butfly[i].length - n; i1++) {
                 butfly[i][i1] = 1;
             }
-            n -= 1;
+            n += 1;
         }
 
         //Step 3: Printing the array filled in the form of butterfly
